ab5efe57655578627956aa79866a946b6e12ec06
Stephen Beitzel <sbeitzel@pobox.com>
2025-01-29 21:28:52 -0800

    Move to document based storage instead of global app storage.

--- Chital/ChitalApp.swift
+++ Chital/ChitalApp.swift
@@ -3,26 +3,13 @@ import SwiftData
 
 @main
 struct ChitalApp: App {
-    var container: ModelContainer = {
-        let configuration = ModelConfiguration(isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: Schema(versionedSchema: ActiveSchema.self),
-                                      configurations: [configuration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-    
     var body: some Scene {
-        WindowGroup {
+        DocumentGroup(editing: .chitalDocument,
+                      migrationPlan: MigrationPlan.self,
+                      editor: {
             ContentView()
-        }
-        .modelContainer(container)
-        .commands {
-            CommandGroup(replacing: .saveItem) {}
-        }
-        
+        })
+
         Settings {
             SettingsView()
         }

196e06e54c7cceb5a5369ff3c03ddee080d4d2ed
Stephen Beitzel <sbeitzel@pobox.com>
2025-01-29 19:35:22 -0800

    Add document type for us to store the threads in.

---
+++ Chital/UTType+Chital.swift
@@ -0,0 +1,12 @@
+//
+//  UTType-Chital.swift
+//  Chital
+//
+//  Created by Stephen Beitzel on 1/29/25.
+//
+
+import UniformTypeIdentifiers
+
+extension UTType {
+    static var chitalDocument = UTType(exportedAs: "com.sheshbabu.Chital.document")
+}
